e9bf4f3bc1a043a214fd6ccea3dda0a7f96a52c3
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-13 18:47:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,33 @@
 def oxford_comma(array)
 
+  new_array = array
+  
+  if new_array.size == 1 
+    oxford_string = new_array.join
+    
+  elsif new_array.size == 2 
+    oxford_string = new_array.join(" and ")
+  
+  else
+    
+    ins_and_index = array.size - 1
+    counter = 0
+    
+    while counter != (new_array.size - 1)
+      
+      new_array[counter] << ", "
+      counter += 1 
+      
+    end
+    
+    new_array.insert(ins_and_index, "and ")
+    oxford_string = new_array.join
+  
+  end
+  
+  # Return oxford_string
+  oxford_string
+  
 end
+
+
